4cd3985cdd76d3bdb10f7f88d1f2faa80c43a059
王深 <wangshen1@yingu.com>
2019-04-09 11:32:44 +0800

    添加tinymce富文本编辑器

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,23 +1,53 @@
 <template>
- <div>
-   资讯管理模块
- </div>
+<div class="createPost-main-container">
+  <div class="editor-container">
+    <Tinymce ref="editor" :height="400" v-model="postForm.content" />
+  </div>
+</div>
 </template>
 
 <script>
- export default {
-   data () {
-     return {
+import Tinymce from '@/components/Tinymce'
 
-     }
-   },
-   components: {
+const defaultForm = {
+  status: 'draft',
+  title: '', // 文章题目
+  content: '', // 文章内容
+  content_short: '', // 文章摘要
+  source_uri: '', // 文章外链
+  image_uri: '', // 文章图片
+  display_time: undefined, // 前台展示时间
+  id: undefined,
+  platforms: ['a-platform'],
+  comment_disabled: false,
+  importance: 0
+}
 
-   }
- }
+export default {
+  data () {
+    return {
+      postForm: Object.assign({}, defaultForm),
+    }
+  },
+  components: {
+    Tinymce
+  }
+}
 </script>
 
 <style lang='scss' scoped>
-
-
+.createPost-main-container {
+  padding: 40px 45px 20px 50px;
+  .editor-container {
+    min-height: 500px;
+    margin: 0 0 30px;
+    .editor-upload-btn-container {
+      text-align: right;
+      margin-right: 10px;
+      .editor-upload-btn {
+        display: inline-block;
+      }
+    }
+  }
+}
 </style>
